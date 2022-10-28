89110c968e10f9488811452e363ad99e25a651ef
Denilson <denuariua@gmail.com>
2022-10-28 13:19:31 +0200

    fixed antd css showing error

--- client/src/components/navigation/Footer.js
+++ client/src/components/navigation/Footer.js
@@ -1,10 +1,6 @@
 import Box from "@mui/material/Box";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
-import IconButton from "@mui/material/IconButton";
-import MenuIcon from "@mui/icons-material/Menu";
-import SearchIcon from "@mui/icons-material/Search";
-import MoreIcon from "@mui/icons-material/MoreVert";
 
 export function Footer() {
   return (
