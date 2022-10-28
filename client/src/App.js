c3cac5f5639b5d6983eb05fb770675d01ed158ca
Denilson <denuariua@gmail.com>
2022-10-28 10:46:52 +0200

    added client

--- client/src/App.js
+++ client/src/App.js
@@ -16,7 +16,7 @@ function App() {
   const [user, setUser] = useState(null);
   // client-side
   socket.on("connect", () => {
-    console.log(socket.id); // x8WIv7-mJelg7on_ALbx
+    console.log(socket.id);
   });
   useEffect(() => {
     localStorage.getItem("user") &&
